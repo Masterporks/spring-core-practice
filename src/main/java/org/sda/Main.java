31750d367bdbd0e2eab532784fd31079082b88af
root <joosepkorela@gmail.com>
2023-02-20 20:45:50 +0200

    xml config setup

--- src/main/java/org/sda/Main.java
+++ src/main/java/org/sda/Main.java
@@ -1,11 +1,21 @@
 package org.sda;
 
-/**
- * @author Joosep Korela
- *
- */
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import org.sda.beans.MyBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+    /**
+     * @author Joosep Korela
+     *
+     */
+    public class Main {
+        public static void main(String[] args) {
+            ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+            MyBean myBean = (MyBean) applicationContext.getBean("myBean");
+            System.out.println(myBean.sayHello());
+
+            myBean.setName("John");
+            System.out.println(myBean.sayHello());
+        }
     }
-}
